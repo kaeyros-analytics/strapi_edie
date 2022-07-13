93c5f175a8359add665724410596cdeb6c84eb79
FrancenelleDigit <francenelle.tankeu@kaeyros-analytics.com>
2022-07-13 16:58:56 +0100

    Update database config

--- config/database.js
+++ config/database.js
@@ -1,11 +1,20 @@
-const path = require('path');
+
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'ec2-3-222-74-92.compute-1.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'daoilq9k55mh41'),
+      user: env('DATABASE_USERNAME', 'foeezsdbhqflly'),
+      password: env('DATABASE_PASSWORD', '57fc06f44eb80cdabadb63dbd272ccb22205d21e364ad1272dc7af127a8fe31f'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
+
--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  proxy: true,
+  url: env('https://calm-journey-51634.herokuapp.com/'),
+  app: { 
+    keys: env.array('APP_KEYS')
   },
-});
+})
